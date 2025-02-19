c1ebe46f69a3534c3c4e7c2a9d1750454a10464b
Yang Hu <yang.h@nyu.edu>
2025-02-19 14:04:43 -0500

    eb setup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c(mg-5swaerv*c+jyqq8#a-#*8u9rq&d)os4)#d=(6oh19-!6u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polls-env.eba-dcbbmcsx.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition
